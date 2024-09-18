47d081c4fd5ce9ad66d3aea37379568c4b9690b3
Your Name <mennaayman2134@gmail.com>
2024-09-18 05:23:20 +0300

    version-1

---
+++ .history/JS/main_20240918050833.js
@@ -0,0 +1,18 @@
+var arr=['Be yourself everyone else is already taken',' So many books so little time.',
+'You only live once, but if you do it right, once is enough','Be the change that you wish to see in the world.',
+' If you tell the truth, you dont have to remember anything.',
+'A friend is someone who knows all about you and still loves you.',
+'To live is the rarest thing in the world. Most people exist, that is all.',
+'Without music, life would be a mistake.',
+'I am so clever that sometimes I dont understand a single word of what I am saying'
+]
+
+var container = document.getElementById('qoute');
+var random=Math.floor(Math.random()* arr.length);
+function addQoute(){
+    if(container === arr[random] ){
+        random=Math.floor(Math.random()* arr.length)
+    }
+    
+    document.getElementById('qoute').innerHTML=arr[random];
+}
